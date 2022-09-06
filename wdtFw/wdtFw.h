d1b35890f8d0c1f325419481b5b794c180b5d17d
ghmayakyan <gevorg.hmayakyan@gmail.com>
2022-09-07 00:08:16 +0400

    Initial Commit

---
+++ wdtFw/wdtFw.h
@@ -0,0 +1,18 @@
+//
+//  wdtFw.h
+//  wdtFw
+//
+//  Created by Gevorg Hmayakyan on 07.09.22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for wdtFw.
+FOUNDATION_EXPORT double wdtFwVersionNumber;
+
+//! Project version string for wdtFw.
+FOUNDATION_EXPORT const unsigned char wdtFwVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <wdtFw/PublicHeader.h>
+
+
